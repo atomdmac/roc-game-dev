4b60c88b3a7c4919991d27d20fd8ed9b43780279
atomdmac <atom.mac@gmail.com>
2016-11-11 23:23:59 -0500

    Initial commit of module w/ spec.
    Signed-off-by: atomdmac <atom.mac@gmail.com>

---
+++ modules/twitter-aggregator.js
@@ -0,0 +1,86 @@
+var Promize = require('promise');
+var Twitter = require('twitter');
+var fs = require('fs');
+
+var TwitterAggregator = function (options) {
+  this.client = new Twitter(options);
+};
+
+TwitterAggregator.prototype.getLocal = function(file) {
+  return new Promize(function (fulfill, reject) {
+    fs.readFile(file, {encoding: 'utf8'}, function (error, data) {
+      if(error === null)
+        // Successfully loaded JSON data.  Now let's parse it!
+        try {
+          data = JSON.parse(data);
+          fulfill(data);
+        } catch (error) {
+          reject(error);
+        }
+      else reject(error);
+    });
+  });
+};
+
+TwitterAggregator.prototype.getRemote = function (searchParams) {
+  var self = this;
+  return new Promize(function (fulfill, reject) {
+    self.client.get(
+      'search/tweets',
+      searchParams,
+      function(error, results, response) {
+        // Report error if necessary.
+        if(error) {
+          reject(error);
+        } else {
+          fulfill(results.statuses);
+        }
+    });
+  });
+};
+
+TwitterAggregator.prototype.refreshLocal = function (file, remoteSearchParams) {
+  var self = this;
+  return new Promize(function(fulfill, reject) {
+    Promize
+      .all([self.getLocal(file), self.getRemote(remoteSearchParams)])
+      .then(
+        function (data) {
+          var localData = data[0];
+          var remoteData = data[1];
+          var combinedData = self.combine(localData, remoteData);
+
+          // Write data to the filesystem.
+          fs.writeFile(file + 'test', JSON.stringify(combinedData, null, 2), {encoding: 'utf8'}, function (err) {
+            if(err === null) {
+              fulfill({
+                combined: combinedData,
+                local: localData,
+                remote: remoteData
+              });
+            } else {
+              reject(err);
+            }
+          });
+        },
+        function (error) {
+          reject(error);
+        }
+      );
+  });
+};
+
+TwitterAggregator.prototype.combine = function (local, remote) {
+  var newData = local.concat(remote);
+  newData = newData.filter(function (tweet, index, tweets) {
+    for(var t = index+1; t<tweets.length; t++) {
+      if(tweet.id === tweets[t].id) {
+        return false;
+      }
+    }
+    return true;
+  });
+  return newData;
+};
+
+module.exports = TwitterAggregator;
---
+++ spec/twitter-aggregator-spec.js
@@ -0,0 +1,66 @@
+var expect = require('chai').expect;
+var TwitterAggregator = require('../modules/twitter-aggregator');
+
+// We're using an external version of the Promise object since the version of
+// node currently running on our server doesn't have a native version yet.
+var Promize = require('promise');
+
+describe('TwitterAggregator', function () {
+
+  // Import some Twitter API credentials to use for our tests.
+  var credentials = require('../credentials/twitter-feed-credentials.json');
+
+  // Create an instance of the Twitter Aggregator to test.
+  var taInstance = new TwitterAggregator(credentials);
+
+  it('Should exist', function () {
+    expect(TwitterAggregator).to.not.equal(undefined);
+  });
+
+  it('Should read existing Twitter data from disk', function () {
+    return taInstance
+      .getLocal('../data_cache/twitter.json')
+      .then(
+        // Success
+        function(localData) {
+          expect(localData, 'Read local Twitter data from disk.').to.be.instanceof(Array);
+        },
+
+        // Fail
+        function(error) {
+          expect(false, 'Failed to load local Twitter data.');
+        }
+      );
+  });
+
+  it('Should read data from the Twitter Search API', function () {
+    return taInstance
+      .getRemote({q:'#rocgamedev'})
+      .then(
+        // Success
+        function(remoteData) {
+          expect(remoteData, 'Read remote data from Twitter Search API').to.be.instanceof(Array);
+        },
+
+        // Fail
+        function(error) {
+          expect(false).to.equal(true, 'Failed to load remote Twitter data.');
+        });
+  });
+
+  it('Should combine local and remote Tweets without duplicates', function () {
+    return taInstance
+      .refreshLocal('./twitter-aggregator-spec-data.js', {q:'#rocgamedev'})
+      .then(
+        // Success
+        function (data) {
+          // Are combined appropriately.
+          expect(data.combined.length).to.equal(data.remote.length + 1);
+        },
+
+        // Failure
+        function (error) {
+          expect(false);
+        });
+  });
+});
