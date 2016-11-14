3b3218d28b32facefab1bf2b4b47898893e4cb61
atomdmac <atom.mac@gmail.com>
2016-11-14 15:12:28 -0500

    Update twitter-feed module to use twitter-aggregator module.
    Signed-off-by: atomdmac <atom.mac@gmail.com>

--- modules/twitter-feed.js
+++ modules/twitter-feed.js
@@ -1,8 +1,7 @@
-var Twitter = require('twitter');
+var TwitterAggregator = require('./twitter-aggregator');
 var Promize = require('promise');
 
 var settings = {
-  // refreshRate: 120000,
   refreshRate: 12000000,
   searchParams: {
     q: '#rocgamedev'
@@ -15,36 +14,16 @@ var refreshInt;
 // Cache of tweets.
 var tweetCache = [];
 
-// Create a Twitter client object to interact with the API.
+// Create a TwitterAggregator object to interact with the API.
 var clientCredentials = require('../credentials/twitter-feed-credentials.json');
-var client = new Twitter(clientCredentials);
+var client = new TwitterAggregator(clientCredentials);
 
 function refresh () {
-  var p = new Promize(function (fulfill, reject) {
-    client.get(
-      'search/tweets',
-      settings.searchParams,
-      function(error, results, response) {
-        // Report error if necessary.
-        if(error) {
-          reject(error);
-          return;
-        }
-
-        // Empty tweets list and start over.
-        tweetCache.splice(0, tweetCache.length -1);
-
-        // Build new list of tweets
-        results.statuses.forEach(function (tweet, index) {
-          tweetCache.push(tweet);
-        });
-
-        fulfill(tweetCache);
+  return client.refresh().then(
+    // Success
+    function (data) {
+      tweetCache = data.combined;
     });
-  });
-
-  // Return promise so we can all get on with our lives.
-  return p;
 }
 
 function start () {
