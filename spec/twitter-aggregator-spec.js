80226ed8e78f16a58d2f7052364b65efae7d1c5b
atomdmac <atom.mac@gmail.com>
2016-12-10 16:34:20 -0500

    Update indents in spec files.
    Signed-off-by: atomdmac <atom.mac@gmail.com>

--- spec/twitter-aggregator-spec.js
+++ spec/twitter-aggregator-spec.js
@@ -8,118 +8,118 @@ var Promize = require('promise');
 
 describe('TwitterAggregator', function () {
 
-  // Import some Twitter API credentials to use for our tests.
-  var credentials = require('../credentials/twitter-feed-credentials.json');
+	// Import some Twitter API credentials to use for our tests.
+	var credentials = require('../credentials/twitter-feed-credentials.json');
 
-  // Create an instance of the Twitter Aggregator to test.
-  var taInstance = new TwitterAggregator(credentials);
+	// Create an instance of the Twitter Aggregator to test.
+	var taInstance = new TwitterAggregator(credentials);
 
-  it('Should exist', function () {
-    expect(TwitterAggregator).to.not.equal(undefined);
-  });
+	it('Should exist', function () {
+		expect(TwitterAggregator).to.not.equal(undefined);
+	});
 
-  it('getLocal', function () {
-    return taInstance
-      .getLocal('./data_cache/twitter.json')
-      .then(
-        // Success
-        function(localData) {
-          expect(localData).to.be.instanceof(Array);
-        },
+	it('getLocal', function () {
+		return taInstance
+			.getLocal('./data_cache/twitter.json')
+			.then(
+				// Success
+				function(localData) {
+					expect(localData).to.be.instanceof(Array);
+				},
 
-        // Fail
-        function(error) {
-          throw new Error(error.message);
-        }
-      );
-  });
+				// Fail
+				function(error) {
+					throw new Error(error.message);
+				}
+			);
+	});
 
-  it('getRemote', function () {
-    return taInstance
-      .getRemote({q:'#rocgamedev'})
-      .then(
-        // Success
-        function(remoteData) {
-          expect(remoteData).to.be.instanceof(Array);
-        },
+	it('getRemote', function () {
+		return taInstance
+			.getRemote({q:'#rocgamedev'})
+			.then(
+				// Success
+				function(remoteData) {
+					expect(remoteData).to.be.instanceof(Array);
+				},
 
-        // Fail
-        function(error) {
-          throw new Error(error.message);
-        });
-  });
+				// Fail
+				function(error) {
+					throw new Error(error.message);
+				});
+	});
 
-  it('getCombined', function () {
-    return taInstance
-      .getCombined('./spec/twitter-aggregator-spec-data.json', {q:'#rocgamedev'})
-      .then(
-        // Success
-        function (data) {
-          // Are combined appropriately.
-          expect(data).to.have.property('local');
-          expect(data).to.have.property('remote');
-          expect(data).to.have.property('combined');
-        },
+	it('getCombined', function () {
+		return taInstance
+			.getCombined('./spec/twitter-aggregator-spec-data.json', {q:'#rocgamedev'})
+			.then(
+				// Success
+				function (data) {
+					// Are combined appropriately.
+					expect(data).to.have.property('local');
+					expect(data).to.have.property('remote');
+					expect(data).to.have.property('combined');
+				},
 
-        // Failure
-        function (error) {
-          throw new Error(error.message);
-        });
-  });
+				// Failure
+				function (error) {
+					throw new Error(error.message);
+				});
+	});
 
-  describe('refresh', function () {
+	describe('refresh', function () {
 
-    var pathToTweetCache = './spec/twitter-aggregator-spec-data.json';
-    var pathToModifiedTweetCache = './spec/twitter-aggregator-data.output.json';
+		var pathToTweetCache = './spec/twitter-aggregator-spec-data.json';
+		var pathToModifiedTweetCache = './spec/twitter-aggregator-data.output.json';
 
-    it('Should save data to specified output file', function () {
-      return taInstance
-        .refresh(
-          // Local input file
-          pathToTweetCache,
+		it('Should save data to specified output file', function () {
+			return taInstance
+				.refresh(
+					// Local input file
+					pathToTweetCache,
 
-          // Remote search parameters
-          {
-            q:'#rocgamedev'
-          },
+					// Remote search parameters
+					{
+						q:'#rocgamedev'
+					},
 
-          // Other options
-          {
-            outputFile: pathToModifiedTweetCache
-          }
-        )
-        .then(
-          // Success
-          function (data) {
-            expect(true).to.equal(true);
-          },
+					// Other options
+					{
+						outputFile: pathToModifiedTweetCache
+					}
+				)
+				.then(
+					// Success
+					function (data) {
+						expect(true).to.equal(true);
+					},
 
-          // Failure
-          function (error) {
-            throw new Error(error.message);
-          });
-    });
+					// Failure
+					function (error) {
+						throw new Error(error.message);
+					});
+		});
 
-    it('Should sort tweets by date (descending) before being saved to disk', function () {
-      // NOTE: We assume that the preveious test (savomg data tp disk) succeeded.
-      var tweets = helpers.getJSON(pathToModifiedTweetCache);
+		it('Should sort tweets by date (descending) before being saved to disk', function () {
+			// NOTE: We assume that the preveious test (savomg data tp disk) succeeded.
+			var tweets = helpers.getJSON(pathToModifiedTweetCache);
 
-      // Should have more than 1 tweet.
-      expect(tweets).to.have.length.above(0);
+			// Should have more than 1 tweet.
+			expect(tweets).to.have.length.above(0);
 
-      var smallestId = null;
-      tweets.forEach(function (item, index) {
-        if(smallestId === null) {
-          smallestId = item.id;
-          return;
-        }
-        if(smallestId > item.id) {
-          smallestId = item.id;
-        } else {
-          throw new Error('Tweets are not sorted by ID (descending) ' + item.id + ' is larger than ' + smallestId);
-        }
-      });
-    });
+			var smallestId = null;
+			tweets.forEach(function (item, index) {
+				if(smallestId === null) {
+					smallestId = item.id;
+					return;
+				}
+				if(smallestId > item.id) {
+					smallestId = item.id;
+				} else {
+					throw new Error('Tweets are not sorted by ID (descending) ' + item.id + ' is larger than ' + smallestId);
+				}
+			});
+		});
 
-  });
+	});
 });
--- spec/twitter-feed-spec.js
+++ spec/twitter-feed-spec.js
@@ -4,62 +4,68 @@ var TwitterFeed = require('../modules/twitter-feed');
 var twitterFeedInstance;
 
 before(function () {
-  // Start polling Twitter data source.
-  twitterFeedInstance = new TwitterFeed({
-    inputFile : './data_cache/twitter.json',
-    outputFile: './data_cache/twitter.json',
-    searchParams: {
-      q: '#rocgamedev'
-    }
-  });
+	// Start polling Twitter data source.
+	twitterFeedInstance = new TwitterFeed({
+		inputFile : './data_cache/twitter.json',
+		outputFile: './data_cache/twitter.json',
+		searchParams: {
+			q: '#rocgamedev'
+		}
+	});
 });
 
 describe('TwitterFeed', function () {
 
-  describe('getTweets (before polling)', function () {
-    it('Should return an empty Array if called before polling has begun.', function () {
-      var tweets = twitterFeedInstance.getTweets();
-      expect(tweets).to.have.length(0);
-    });
-  });
+	describe('getTweets (before polling)', function () {
+		it('Should return an empty Array if called before polling has begun.', function () {
+			var tweets = twitterFeedInstance.getTweets();
+			expect(tweets).to.have.length(0);
+		});
+	});
 
-  describe('getTweets', function () {
+	describe('getTweets', function () {
 
-    before(function () {
-      // Start polling for Tweets.
-      return twitterFeedInstance.start();
-    });
+		before(function () {
+			// Start polling for Tweets.
+			return twitterFeedInstance.start()
+			.then(function () {
+				console.log('resolve: ');
+			});
+		});
 
-    it('Tweets should be sorted by ID (descending)', function () {
+		after(function () {
+			twitterFeedInstance.stop();
+		});
 
-      var tweets = twitterFeedInstance.getTweets();
+		it('Tweets should be sorted by ID (descending)', function () {
+			var tweets = twitterFeedInstance.getTweets();
 
-      tweets.sort(function(a, b) {
-        if(a.id > b.id) return 1;
-        if(a.id < b.id) return -1;
-        return 0;
-      });
+			tweets.sort(function(a, b) {
+				if(a.id > b.id) return 1;
+				if(a.id < b.id) return -1;
+				return 0;
+			});
 
-      var largestId = null;
-      tweets.forEach(function (item, index) {
-        if(largestId === null) {
-          largestId = item.id;
-          return;
-        }
-        if(largestId < item.id) {
-          largestId = item.id;
-        } else {
-          throw new Error('Tweets are not sorted by ID (descending)');
-        }
-      });
-    });
+			var largestId = null;
+			tweets.forEach(function (item, index) {
+				if(largestId === null) {
+					largestId = item.id;
+					return;
+				}
+				if(largestId < item.id) {
+					largestId = item.id;
+				} else {
+					throw new Error('Tweets are not sorted by ID (descending)');
+				}
+			});
+		});
 
-    it('Should return 6 most recent tweets', function () {
-      var tweets = twitterFeedInstance.getTweets(6);
-      expect(tweets.length).to.equal(6);
-    });
+		it('Should return 6 most recent tweets', function () {
+			var tweets = twitterFeedInstance.getTweets(6);
+			expect(tweets.length).to.equal(6);
+		});
 
-    // TODO
-    it('Should return empty tweet list if called before tweets have been returned.');
-  });
+		// TODO
+		it('Should return empty tweet list if called before tweets have been returned.');
+	});
 });
