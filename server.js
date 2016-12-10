cd8413ebb8b2871bd23a96bff68b2890d3ec08b5
atomdmac <atom.mac@gmail.com>
2016-12-10 16:55:14 -0500

    chore(server): Satisfy whiny linter.
    Signed-off-by: atomdmac <atom.mac@gmail.com>

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ var htmlTpl;
 var cmdLineArgs = require('minimist')(process.argv);
 
 // Configure body-parser Express plug-in for using variables from POST requests.
-var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
@@ -32,15 +31,15 @@ var cwd = path.resolve();
 
 // Load HTML templates
 function compileHTMLTemplates () {
-  fs.readFile('./client/build/html/home.html', 'utf8', function (err, data) {
-    htmlTpl = handlebars.compile(data);
-  });
+	fs.readFile('./client/build/html/home.html', 'utf8', function (err, data) {
+		htmlTpl = handlebars.compile(data);
+	});
 }
 
 // DEBUG: If in debug mode...
 if(DEBUG_MODE) {
-  // recompile templates on an interval.
-  setInterval(compileHTMLTemplates, 1000);
+	// recompile templates on an interval.
+	setInterval(compileHTMLTemplates, 1000);
 }
 
 // Compile HTML templates immediately.
@@ -50,58 +49,58 @@ compileHTMLTemplates();
 
 // Home page rendering
 app.get('/', function (req, res) {
-  res.send(htmlTpl(getPageData()));
+	res.send(htmlTpl(getPageData()));
 });
 
 // JavaScript files
 app.get('/js/:fileName', function (req, res) {
-  res.sendFile(cwd + '/client/build/js/' + req.params.fileName);
+	res.sendFile(cwd + '/client/build/js/' + req.params.fileName);
 });
 
 // CSS files
 app.get('/css/:fileName', function (req, res) {
-  res.sendFile(cwd + '/client/build/css/' + req.params.fileName);
+	res.sendFile(cwd + '/client/build/css/' + req.params.fileName);
 });
 
 // Images
 app.get('/images/:fileName', function (req, res) {
-  res.sendFile(cwd + '/client/build/images/' + req.params.fileName);
+	res.sendFile(cwd + '/client/build/images/' + req.params.fileName);
 });
 
 // Facebook Event Updates
 app.get('/update', function (req, res) {
-  res.sendFile(cwd + '/client/build/html/update.html');
+	res.sendFile(cwd + '/client/build/html/update.html');
 });
 
 // Accept event update requests.
 app.post('/update', function (req, res) {
-  facebookFeedInstance.refresh(req.body.token)
+	facebookFeedInstance.refresh(req.body.token)
 
-    // Success
-    .then(function (fbResponse) {
-      res.send(fbResponse);
-      logger.info('Facebook event data updated successfully.');
-    },
+		// Success
+		.then(function (fbResponse) {
+			res.send(fbResponse);
+			logger.info('Facebook event data updated successfully.');
+		},
 
-    // Failure
-    function (reason) {
-      logger.error('Facebook event data upate failure!.');
-      res.send(reason);
-    });
+		// Failure
+		function (reason) {
+			logger.error('Facebook event data upate failure!.');
+			res.send(reason);
+		});
 });
 
 // Start polling Twitter data source.
 var twitterFeedInstance = new TwitterFeed({
-  inputFile : './data_cache/twitter.json',
-  outputFile: './data_cache/twitter.json',
-  searchParams: {
-    q: '#rocgamedev'
-  }
+	inputFile : './data_cache/twitter.json',
+	outputFile: './data_cache/twitter.json',
+	searchParams: {
+		q: '#rocgamedev'
+	}
 });
 
 var facebookFeedInstance = new FacebookFeed({
-  cacheLocation: './data_cache/events.json',
-  groupId: '1453415071632653'
+	cacheLocation: './data_cache/events.json',
+	groupId: '1453415071632653'
 });
 
 // Start polling!
@@ -109,46 +108,22 @@ twitterFeedInstance.start();
 
 // HTTP server
 app.listen(PORT, function () {
-  logger.info('ROC Game Dev server listening on port', PORT);
+	logger.info('ROC Game Dev server listening on port', PORT);
 });
 
 // Return all aggregated page data.
 function getPageData () {
-  return {
-    tweets: twitterFeedInstance.getTweets(12),
-    events: facebookFeedInstance.getEvents()
-  };
-}
-
-// Stub for generating FB event data (since we're not currently hooked into FB)
-function getEvents () {
-  return [
-    {
-      eventName: 'September Workshop',
-      eventDate: 'Wednesday September 7th, 2016',
-      eventLocation: 'RIT MAGIC',
-      eventDetails: 'This is where we might put directions for finding the place and what people can expect to find when they get there.'
-    },
-    {
-      eventName: 'September Social',
-      eventDate: 'Wednesday September 28th, 2016',
-      eventLocation: 'Swillburger',
-      eventDetails: 'This is where we might put directions for finding the place and what people can expect to find when they get there.'
-    },
-    {
-      eventName: 'October Workshop',
-      eventDate: 'Wednesday October 5th, 2016',
-      eventLocation: 'RIT MAGIC',
-      eventDetails: 'This is where we might put directions for finding the place and what people can expect to find when they get there.'
-    }
-  ];
+	return {
+		tweets: twitterFeedInstance.getTweets(12),
+		events: facebookFeedInstance.getEvents()
+	};
 }
 
 // Inserts content if the first number is larger than the second.
 handlebars.registerHelper('is_larger_than', function(number1, number2, options) {
-  if( number1 > number2 ) {
-      return options.fn(this);
-  } else {
-      return options.inverse(this);
-  }
+	if( number1 > number2 ) {
+			return options.fn(this);
+	} else {
+			return options.inverse(this);
+	}
 });
