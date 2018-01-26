94c4de962c0f1c6a57674807e79f204d8ea382b4
atomdmac <atom.mac@gmail.com>
2018-01-26 18:37:01 -0500

    chore: Update indents from tabs to spaces

--- modules/twitter-feed.js
+++ modules/twitter-feed.js
@@ -3,75 +3,75 @@ var extend = require('extend');
 var logger = require('winston');
 
 var defaultOptions = {
-	inputFile: './twitter-input.json',
-	outputFile: './twitter-output.json',
-	searchParams: {
-		q: '#twitter'
-	},
-	refreshRate: 60000
+  inputFile: './twitter-input.json',
+  outputFile: './twitter-output.json',
+  searchParams: {
+    q: '#twitter'
+  },
+  refreshRate: 60000
 };
 
 function TwitterFeed (options) {
-	// Merge the provided options in with the defaults.
-	this.options = extend(true, {}, defaultOptions, options);
+  // Merge the provided options in with the defaults.
+  this.options = extend(true, {}, defaultOptions, options);
 
-	// An in-memory cache of the Tweets that have been retreived.
-	this.cache = [];
+  // An in-memory cache of the Tweets that have been retreived.
+  this.cache = [];
 
-	// Store the refresh timeout ID so we can clear it if necessary.
-	this.refreshInt = null;
+  // Store the refresh timeout ID so we can clear it if necessary.
+  this.refreshInt = null;
 
-	// Create a TwitterAggregator object to interact with the API.
-	var clientCredentials = require('../credentials/twitter-feed-credentials.json');
-	this.twitterAggregator = new TwitterAggregator(clientCredentials);
+  // Create a TwitterAggregator object to interact with the API.
+  var clientCredentials = require('../credentials/twitter-feed-credentials.json');
+  this.twitterAggregator = new TwitterAggregator(clientCredentials);
 }
 
 TwitterFeed.prototype.refresh = function () {
-	var self = this;
-	return this.twitterAggregator.refresh(
-		this.options.inputFile,
-		this.options.searchParams,
-		{
-			outputFile: this.options.outputFile
-		})
-	.then(
-		// Success
-		function (data) {
-			// Make a clone of the returned combined data.
-			self.cache = data.combined.slice(0);
-			return data;
-		},
+  var self = this;
+  return this.twitterAggregator.refresh(
+    this.options.inputFile,
+    this.options.searchParams,
+    {
+      outputFile: this.options.outputFile
+    })
+    .then(
+      // Success
+      function (data) {
+        // Make a clone of the returned combined data.
+        self.cache = data.combined.slice(0);
+        return data;
+      },
 
-		// Failure
-		function (reason) {
-			logger.error('TwitterAggregator failed: ', reason);
-			return reason;
-		});
+      // Failure
+      function (reason) {
+        logger.error('TwitterAggregator failed: ', reason);
+        return reason;
+      });
 };
 
 TwitterFeed.prototype.start = function () {
-	var self = this;
-	this.refreshInt = setInterval(
-		function () {
-			self.refresh();
-		},
-		this.options.refreshRate
-	);
-	return this.refresh();
+  var self = this;
+  this.refreshInt = setInterval(
+    function () {
+      self.refresh();
+    },
+    this.options.refreshRate
+  );
+  return this.refresh();
 };
 
 TwitterFeed.prototype.stop = function () {
-	clearInterval(this.refreshInt);
+  clearInterval(this.refreshInt);
 };
 
 TwitterFeed.prototype.getTweets = function (maxTweets) {
-	if(typeof maxTweets === 'number' && maxTweets > 0) {
-		return this.cache.slice(0, maxTweets);
-	}
+  if(typeof maxTweets === 'number' && maxTweets > 0) {
+    return this.cache.slice(0, maxTweets);
+  }
 
-	else {
-		return this.cache.slice();
-	}
+  else {
+    return this.cache.slice();
+  }
 };
 
 module.exports = TwitterFeed;
