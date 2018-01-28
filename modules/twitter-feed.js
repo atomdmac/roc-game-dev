5efb6eaae8aadfab9351fb1cde27478b3bf624de
atomdmac <atom.mac@gmail.com>
2018-01-28 17:40:13 -0500

    feat: pull Twitter creds from env

--- modules/twitter-feed.js
+++ modules/twitter-feed.js
@@ -22,7 +22,12 @@ function TwitterFeed (options) {
   this.refreshInt = null;
 
   // Create a TwitterAggregator object to interact with the API.
-  var clientCredentials = require('../credentials/twitter-feed-credentials.json');
+  var clientCredentials = {
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  };
   this.twitterAggregator = new TwitterAggregator(clientCredentials);
 }
 
